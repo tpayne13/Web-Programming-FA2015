728f4d4602e55dccbd00dcc624b2ed2e494899dd
Steven Yackel <spazard1@gmail.com>
2015-10-13 10:09:09 -0500

    Adds assignment 6

---
+++ Assignments/Assignment6_DependencyInjection/Assignment6/src/Assignment6/Controllers/HobbitController.cs
@@ -0,0 +1,52 @@
+﻿using Assignment6.Filters;
+using Assignment6.Services;
+using Microsoft.AspNet.Mvc;
+using System.Collections.Generic;
+
+namespace Assignment6.Controllers
+{
+	/*
+	* The main endpoint controller. Stores and allows updates to a list of strings (hobbit names).
+	*/
+	[Route("api/[controller]")]
+	[TypeFilter(typeof(StopwatchFilter))]
+	[TypeFilter(typeof(RequestIdFilter))]
+	public class HobbitController : Controller
+	{
+		/*
+		* TODO: Get the MemoryDatabase from DependencyInjection instead.
+		*/
+		private MemoryDatabase database = new MemoryDatabase();
+
+		/*
+		* TODO: Get the StopwatchService from DependencyInjection instead.
+		*/
+		private StopwatchService watchService = new StopwatchService();
+
+		[HttpGet]
+		public IEnumerable<string> Get()
+		{
+			/*
+			* TODO: Shouldn't be using ConsoleLogger directly. What if we wanted to use a different type of logger?
+			*/
+			ConsoleLogger.Instance.Log("GET hobbits returning " + database.Size);
+			watchService.Lap("Controller");
+
+			return database.GetData("Hobbit");
+		}
+
+		[HttpPost]
+		public string Post([FromQuery] string hobbit)
+		{
+			/*
+			* TODO: Shouldn't be using ConsoleLogger directly. What if we wanted to use a different type of logger?
+			*/
+			ConsoleLogger.Instance.Log("POST hobbits adding " + hobbit);
+			watchService.Lap("Controller");
+
+			database.AddString("Hobbit", hobbit);
+
+			return hobbit;
+		}
+	}
+}
