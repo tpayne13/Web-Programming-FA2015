7effd70ed781806b35880dfd775bcaac3a26ab4b
spazard1 <spazard1@gmail.com>
2015-10-14 20:08:58 -0500

    lecture for 10/15

---
+++ Lectures/10-15-2015 filters and middlware/LordOfTheRings/src/LordOfTheRings/Controllers/RingsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using LordOfTheRings.Services;
+using System.Threading;
+
+namespace LordOfTheRings.Controllers
+{
+    [Route("api/[controller]")]
+    public class RingsController : Controller
+    {
+		private ILoggingService logger;
+
+		public RingsController(ILoggingService logger)
+		{
+			this.logger = logger;
+		}
+
+        [HttpGet]
+        public Ring Get()
+        {
+			logger.Log("Called GET!");
+
+			return new Ring()
+			{
+				Name = "Gandalf's Ring",
+				Set = "Elves"
+			};
+        }
+
+        
+		public class Ring
+		{
+			public string Name { get; set; }
+			public string Set { get; set; }
+		}
+    }
+}
